bdcb8169dfcabea7947360431d89ffb8f0d5145c
bhishmdt <bhishmdt@gmail.com>
2021-06-28 17:16:49 +0530

    Test app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Locations from './components/Location';
+import LocationLoadingComponent from './components/LocationLoading'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const LocationLoading = LocationLoadingComponent(Locations);
+	const [appState, setAppState] = useState({
+		loading: false,
+		locations: null,
+	});
 
+	useEffect(() => {
+		setAppState({ loading: true });
+		const apiUrl = `http://127.0.0.1:8000/manufacturing/`;
+		fetch(apiUrl)
+			.then((data) => data.json())
+			.then((locations) => {
+        console.log(locations)
+				setAppState({ loading: false, locations: locations });
+			});
+	}, [setAppState]);
+	return (
+		<div className="App">
+			<h1>Locations</h1>
+			<LocationLoading isLoading={appState.loading} location={appState.locations} />
+		</div>
+	);
+}
 export default App;
